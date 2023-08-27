710e641e5a7b373a4b79474a48c9bc01f97bb335
JKnight3517 <knightjknight@gmail.com>
2023-08-26 17:25:17 -0700

    Clean up the order view

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var orders: [Int] = [1,2,3,4,5,6]
-    var showOrders: Bool = false
+    @State private var showOrders: Bool = true
     
     
     var body: some View {
         VStack {
             HeaderView()
                 .shadow(radius: 5)
+
+
+            .foregroundStyle(.white)
             if showOrders {
                 OrderView(orders: orders)
+                    .cornerRadius(10)
             } else {
                 MenuItemView()
                     .padding(5)
--- HuliPizza/Views/OrderRowView.swift
+++ HuliPizza/Views/OrderRowView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct OrderRowView: View {
     var order: Int
+    var quantity: Int = 1
     var body: some View {
-        HStack(alignment: .firstTextBaseline) {
-            Text("Your Order Item \(order)")
-            Spacer()
-            Text(19.90, format: .currency(code: "USD"))
+        
+        VStack {
+            HStack {
+                Text("Your Order Item \(order)")
+                Spacer()
+            }
+            HStack(alignment: .firstTextBaseline) {
+                Text("\(quantity) @")
+                Text(19.90, format: .currency(code: "USD"))
+                Spacer()
+                Text(19.90, format: .currency(code: "USD"))
+                    .fontWeight(.semibold)
+            }
         }
     }
 }
