d1f178397afcf0e01a52821e95c45d9dd2dfe434
JKnight3517 <knightjknight@gmail.com>
2023-08-29 09:02:08 -0700

    Update header view to work in landscape

--- HuliPizza/Views/HeaderView.swift
+++ HuliPizza/Views/HeaderView.swift
@@ -8,22 +8,54 @@
 import SwiftUI
 
 struct HeaderView: View {
+    @EnvironmentObject var orders: OrderModel
+    @Environment(\.verticalSizeClass) var vSizeClass: UserInterfaceSizeClass?
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            Image("surfBanner")
-                .resizable()
-                .scaledToFit()
-            Text("Huli Pizza Company")
-                .font(.custom("Georgia", size: 30, relativeTo: .title))
-                .foregroundStyle(.regularMaterial)
-                .fontWeight(.semibold)
+        VStack {
+            if (vSizeClass ?? .regular) != .compact {
+                ZStack(alignment: .bottomTrailing) {
+                    Image("surfBanner")
+                        .resizable()
+                        .scaledToFit()
+                    Text("Huli Pizza Company")
+                        .font(.custom("Georgia", size: 30, relativeTo: .title))
+                        .foregroundStyle(.regularMaterial)
+                        .fontWeight(.semibold)
+                    
+                }
+                
+            } else {
+                HStack(alignment: .bottom) {
+                    Image("surfBanner")
+                        .resizable()
+                        .scaledToFit()
+                    Text("Huli Pizza Company")
+                        .font(.custom("Georgia", size: 30, relativeTo: .title))
+                        .foregroundColor(Color("Surf"))
+                        .fontWeight(.heavy)
+                }
+                    
+            }
+            Label {
+                Text(orders.orderTotal, format: .currency(code: "USD"))
+                
+            }
+        icon: {
+            Image(systemName: orders.orderItems.isEmpty ? "cart" : "cart.circle.fill")
+        }
+ 
             
         }
+        .background(.ultraThinMaterial)
+        
+        
+        
+        
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView().environmentObject(OrderModel())
     }
 }
